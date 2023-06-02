694ecb343f69960a617e0d1419ab8e4a8bb82716
shuayb <shucaybka1439@gmail.com>
2023-06-03 02:35:56 +0300

    Added my project

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -57,3 +57,8 @@
 <script></script>
 
 <style></style>
+git init
+git add -A
+git commit -m 'Added my project'
+git remote add origin git@github.com:sammy/my-new-project.git
+git push -u -f origin main
